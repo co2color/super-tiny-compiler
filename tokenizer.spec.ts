f49cdefa9e9f4f1a12964167901472b377722d52
cococolor <819109255@qq.com>
2023-03-11 01:31:54 +0800

    ✨ feat: tokenizer

---
+++ tokenizer.spec.ts
@@ -0,0 +1,19 @@
+import { test, expect } from 'vitest'
+import { tokenizer, TokenTypes } from './tokenizer'
+
+test('tokenizer', () => {
+  const code = `(add 2 (subtract 4 2))`
+  const tokens = [
+    { type: TokenTypes.Paren, value: '(' },
+    { type: TokenTypes.Name, value: 'add' },
+    { type: TokenTypes.Number, value: '2' },
+    { type: TokenTypes.Paren, value: '(' },
+    { type: TokenTypes.Name, value: 'subtract' },
+    { type: TokenTypes.Number, value: '4' },
+    { type: TokenTypes.Number, value: '2' },
+    { type: TokenTypes.Paren, value: ')' },
+    { type: TokenTypes.Paren, value: ')' },
+  ]
+
+  expect(tokenizer(code)).toEqual(tokens)
+})
